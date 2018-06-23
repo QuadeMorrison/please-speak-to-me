6a3e745061e5c2c62419089a02ea044d61049a0b
quademorrison <quademorrison@gmail.com>
2018-06-23 13:58:55 -0600

    Few changes so vue-router works

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const express = require('express')
+const history = require('connect-history-api-fallback')
+const app = express()
+// Express server.
+const staticFileMiddleware = express.static(__dirname + "/dist")
+app.use(staticFileMiddleware)
+app.use(history({
+ disableDotRule: true,
+ verbose: true
+}))
+app.use(staticFileMiddleware)
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.listen(port, () => {
+ console.log('App listening on port ${5555}!')
+})
