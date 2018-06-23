148748428660d27cd8d34ea492be9a960ffdc4bc
quademorrison <quademorrison@gmail.com>
2018-06-23 14:08:43 -0600

    Reverting changes

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express')
-const history = require('connect-history-api-fallback')
-const app = express()
-// Express server.
-const staticFileMiddleware = express.static(__dirname + "/dist")
-app.use(staticFileMiddleware)
-app.use(history({
- disableDotRule: true,
- verbose: true
-}))
-app.use(staticFileMiddleware)
-var port = process.env.PORT || 5000;
+const path = require('path')
+const serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(__dirname + "/dist"))
+const port = process.env.PORT || 5000
 app.listen(port, () => {
- console.log('App listening on port ${5555}!')
+    console.log('server started '+ port)
 })
