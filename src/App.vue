cab025ea01d4b2318346055f1cfddb89dff8c5a3
quademorrison <quademorrison@gmail.com>
2018-06-23 15:21:02 -0600

    Showing off aesthetic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/speaking.svg" width="20%">
     <router-view/>
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,14 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+<template lang="pug">
+  div
+    input(autofocus type='text' @keyup.enter.native="onSubmit"
+          placeholder='please speak with me')
+    span
+      | Icons made by&nbsp
+      a(href="http://www.freepik.com" title="Freepik") Freepik
+      | &nbspfrom&nbsp
+      a(href="https://www.flaticon.com/" title="Flaticon") www.flaticon.com
+      | &nbspis licensed by&nbsp
+      a(href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank") CC 3.0 BY
 </template>
 
 <script>
@@ -88,7 +16,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      sentenceToGen: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+
     }
   }
 }
@@ -110,4 +43,20 @@ li {
 a {
   color: #42b983;
 }
+
+input {
+  text-align: center;
+  border: none;
+  display: block;
+  font-size: 45px;
+  border-bottom: 4px #35A4ED solid;
+  height: 60px;
+  width: 80%;
+  margin: 50px auto;
+}
+
+*:focus {
+    outline: none;
+}
+
 </style>
