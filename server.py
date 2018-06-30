c30f2356f29a258decd9d9cd1531386f7c663ba1
quademorrison <quademorrison@gmail.com>
2018-06-30 16:02:15 -0600

    changing to python webserver

---
+++ server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask, request, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='./dist')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
